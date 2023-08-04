06363ef833c6dff58d475b2faea34458e62612ce
YasiruWickramasinghe <it19038292@my.sliit.lk>
2023-08-04 12:14:02 +0530

    finalize project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-gray-800 text-sm p-4 text-center text-white">
-      Copyright © 2023 <span className="text-cyan-600">PEARLIZE LAB</span>. All Rights reserved.
+      Copyright © 2023 <a href={`www.yasiruvisions.com`}><span className="text-cyan-600">PEARLIZE LAB</span></a>. All Rights reserved.
     </div>
   );
 };
