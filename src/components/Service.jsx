7e7648c54b7ae09d4c04a7602e759ec14f5a0a59
YasiruWickramasinghe <it19038292@my.sliit.lk>
2023-08-04 14:04:34 +0530

    Service completed

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import project1 from "../assets/images/project-1.png";
-import project2 from "../assets/images/project-2.png";
-import project3 from "../assets/images/project-3.png";
+import service1 from "../assets/images/service1.png";
+import service2 from "../assets/images/service2.png";
+import service3 from "../assets/images/service3.png";
+import service4 from "../assets/images/service4.png";
+import service5 from "../assets/images/service5.png";
+import service6 from "../assets/images/service6.png";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination, Autoplay } from "swiper";
@@ -10,37 +13,37 @@ import { Pagination, Autoplay } from "swiper";
 const Service = () => {
     const projects = [
         {
-            img: project1,
+            img: service1,
             name: "Web Development",
             desc: "I create expertly crafted websites that enhance your online presence and  business growth, functionality with aesthetics.",
             link: "#contact",
         },
         {
-            img: project2,
+            img: service2,
             name: "Mobile App Development",
             desc: "My innovative mobile apps are tailored to captivate users, meet unique requirements, and provide a digital experience.",
             link: "#contact",
         },
         {
-            img: project3,
+            img: service3,
             name: "UI/UX Design",
             desc: "Visually appealing designs ensure seamless user experiences, boost engagement, and make your brand stand out.",
             link: "#contact",
         },
         {
-            img: project3,
+            img: service4,
             name: "POS Systems",
             desc: "Streamline your retail operations with our efficient Point of Sale solutions, enhancing productivity and experience.",
             link: "#contact",
         },
         {
-            img: project3,
+            img: service5,
             name: "SEO",
             desc: "Improve your visibility and attract organic traffic with our data-driven SEO strategies and proven optimization.",
             link: "#contact",
         },
         {
-            img: project3,
+            img: service6,
             name: "Artificial Intelligence",
             desc: "Unlock the power of AI to automate tasks, gain valuable insights, and stay ahead in today's business environment.",
             link: "#contact",
@@ -52,7 +55,7 @@ const Service = () => {
                 <h3 className="text-4xl font-semibold">
                     My <span className="text-cyan-600">Services</span>
                 </h3>
-                <p className="text-gray-400 mt-3 text-lg">Customized Service Packages</p>
+                <p className="text-gray-400 mt-3 text-lg">What I Offer</p>
             </div>
             <br />
             <div className="flex max-w-6xl gap-6 px-5 mx-auto items-center relative">
@@ -78,7 +81,7 @@ const Service = () => {
                             <SwiperSlide key={i}>
                                 <div className="h-fit w-full p-4 bg-slate-700 rounded-xl">
                                     <div className="text-center">
-                                        <h2 className="text-xl my-4 font-semibold">{project_info.name}</h2>
+                                        <h2 className="text-xl my-4 font-semibold ">{project_info.name}</h2>
                                     </div>
                                     <img src={project_info.img} alt="" className="rounded-lg" />
                                     <h5 className="text-l my-4">{project_info.desc}</h5>
