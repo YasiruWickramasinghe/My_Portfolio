976cd86705c6f7cd234be1f07220656afd020bc4
Yasiru Wickramasinghe <78946418+YasiruWickramasinghe@users.noreply.github.com>
2025-01-20 07:51:46 +0530

    Update Copy Right Year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-gray-800 text-sm p-4 text-center text-white">
-      Copyright © 2023 <a href={`www.yasiruvisions.com`}><span className="text-cyan-600">PEARLIZE LAB</span></a>. All Rights reserved.
+      Copyright © 2025 <a href={`www.yasiruvisions.com`}><span className="text-cyan-600">PEARLIZE LAB</span></a>. All Rights reserved.
     </div>
   );
 };
